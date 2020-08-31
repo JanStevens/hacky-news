739aa8ac0655cd72c83394d4cafa7ca125b529db
Jan Stevens <jan@playpass.be>
2020-08-31 11:11:50 +0200

    Add styles provider

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import NavBar from './components/NavBar'
 import Posts from './components/Posts'
 
 import { CssBaseline } from '@material-ui/core'
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles'
 import { ThemeProvider } from './components/ThemeProvider'
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'hn',
+})
 
 const App = () => {
   return (
-    <ThemeProvider>
-      <CssBaseline />
-      <Container maxWidth="md">
-        <NavBar />
-        <Posts />
-      </Container>
-    </ThemeProvider>
+    <StylesProvider generateClassName={generateClassName}>
+      <ThemeProvider>
+        <CssBaseline />
+        <Container maxWidth="md">
+          <NavBar />
+          <Posts />
+        </Container>
+      </ThemeProvider>
+    </StylesProvider>
   )
 }
 
